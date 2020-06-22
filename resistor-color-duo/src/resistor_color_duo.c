f71256e1ab8f38b32fcdf612a636dddcd90190d0
Matthew DeMichele <Matt@Matthews-MacBook-Air.local>
2020-06-22 09:33:53 -1000

    resistor_color_duo done

--- resistor-color-duo/src/resistor_color_duo.c
+++ resistor-color-duo/src/resistor_color_duo.c
@@ -1,1 +1,16 @@
 #include "resistor_color_duo.h"
+#include <stdio.h>
+
+
+resistor_band_t color_code(resistor_band_t *colors) {
+  
+  resistor_band_t c1 = colors[0];
+  resistor_band_t c2 = colors[1];
+  
+  resistor_band_t result = (c1 * 10) + c2;
+
+
+  return (result);
+}
+
+
--- resistor-color-duo/src/resistor_color_duo.h
+++ resistor-color-duo/src/resistor_color_duo.h
@@ -1,4 +1,19 @@
 #ifndef RESISTOR_COLOR_DUO_H
 #define RESISTOR_COLOR_DUO_H
 
+typedef enum {
+  BLACK,
+  BROWN,
+  RED,
+  ORANGE,
+  YELLOW, 
+  GREEN,
+  BLUE,
+  VIOLET,
+  GREY,
+  WHITE,
+} resistor_band_t;
+
+resistor_band_t color_code();
+
 #endif
